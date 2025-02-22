62126c7383bd67eb3ee943bdf920b2cde50bf363
qKitNp <pranjal@pm.me>
2025-02-22 19:34:49 +0530

    friendly for streamlit cloud now

--- embedding.py
+++ embedding.py
@@ -1,13 +1,11 @@
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from dotenv import load_dotenv
 import os
+import streamlit as st
 
 load_dotenv()
 
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
-
-if "GOOGLE_API_KEY" not in os.environ:
-    os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
+GOOGLE_API_KEY = st.secrets["general"]["API_KEY"]
 
 def get_embeddings():
     embeddings = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004")
