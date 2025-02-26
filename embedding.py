0c8c1a50b8fb25f99c5aef6c33d15be2550b906a
qKitNp <pranjal@pm.me>
2025-02-26 17:02:58 +0530

    feat: add logging for embedding retrieval and API key

--- embedding.py
+++ embedding.py
@@ -8,5 +8,7 @@ load_dotenv()
 GOOGLE_API_KEY = st.secrets["API_KEY"]
 
 def get_embeddings():
+    os.write(1, f"Getting embeddings\n".encode())
+    os.write(1, f"API Key: {GOOGLE_API_KEY}\n".encode())
     embeddings = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004")
     return embeddings
